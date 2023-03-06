4fc9529a2bc9c7443fa4402420409142673c43bb
kubaz <jakub.zawadzki16@gmail.com>
2023-03-06 12:25:12 +0100

    castle-security module added, it will be responsible of securing the application using Spring Security.

--- src/main/java/com/castle/security/CastleSecurityApplication.java
+++ src/main/java/com/castle/security/CastleSecurityApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CastleSecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CastleSecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CastleSecurityApplication.class, args);
+    }
 
 }
