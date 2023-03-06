8ca56bc540ac9db807a91cd354c40ff5cd55b442
kubaz <jakub.zawadzki16@gmail.com>
2023-03-06 12:23:03 +0100

    Initial commit

---
+++ src/main/java/com/castle/security/CastleSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.castle.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastleSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastleSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/castle/security/CastleSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castle.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastleSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
